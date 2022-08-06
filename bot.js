eebbba0435aa48a5fbf869ff9208f79fb20b0477
sasiya1 <110380881+sasiya1@users.noreply.github.com>
2022-08-06 10:21:31 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,10 +33,10 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['94714789596',''] //owner number +27634090203
+global.wame = ['94714789596'] 
+global.pemilik = ['94714789596'] //owner number +27634090203
+global.premium = ['94714789596'] //premium number +27634090203
 global.pengguna = 'Drips' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
